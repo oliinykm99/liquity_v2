f87d915f332bc7dce4f1c6705ac3c5eae38ad93a
Oliinyk Maksym <maxim@oliinyk.com.ua>
2025-03-23 14:41:11 -0400

    Updated __init__.py

--- src/core/tasks/aggregate/__init__.py
+++ src/core/tasks/aggregate/__init__.py
@@ -1,3 +1,4 @@
 from .aggregate_pools import aggregate_pools
+from .aggregate_troves import aggregate_troves
 
-__all__=['aggregate_pools']
+__all__=['aggregate_pools', 'aggregate_troves']
--- src/core/tasks/clean/__init__.py
+++ src/core/tasks/clean/__init__.py
@@ -4,7 +4,9 @@ from .clean_activeDEBT import clean_activeDEBT
 from .clean_activeTVL import clean_activeTVL
 from .clean_activeTVL_USD import clean_activeTVL_USD
 from .clean_troveCR import clean_troveCR
+from .clean_troveRequiredData import clean_troveRequiredData
 
 __all__ = ['clean_stabilityTVL', 'clean_prices',
            'clean_activeDEBT', 'clean_activeTVL',
-           'clean_activeTVL_USD', 'clean_troveCR']
+           'clean_activeTVL_USD', 'clean_troveCR',
+           'clean_troveRequiredData']
