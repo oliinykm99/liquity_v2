44b9c7ffcf0f54a99ae9d03763eeeab050c8911e
Oliinyk Maksym <maxim@oliinyk.com.ua>
2025-03-23 14:00:25 -0400

    Updated __init__.py

--- src/core/tasks/extract/__init__.py
+++ src/core/tasks/extract/__init__.py
@@ -2,6 +2,15 @@ from .activeDEBT import fetch_activeDEBT
 from .activeTVL import fetch_activeTVL
 from .price import fetch_price
 from .stabilityTVL import fetch_stabilityTVL
+from .troveSizes import fetch_troveSizes
+from .troveIDs import fetch_troveIDs
+from .troveOwner import fetch_troveOwner
+from .troveCR import fetch_troveCR
+from .troveData import fetch_troveData
+from .troveRequiredData import extract_requiredTroveData
 
 __all__ = ['fetch_activeDEBT','fetch_activeTVL',
-           'fetch_price','fetch_stabilityTVL']
+           'fetch_price','fetch_stabilityTVL',
+           'fetch_troveSizes', 'fetch_troveIDs',
+           'fetch_troveOwner', 'fetch_troveCR',
+           'fetch_troveData', 'extract_requiredTroveData']
