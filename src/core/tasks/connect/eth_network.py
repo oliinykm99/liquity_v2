508df8cc0fdb0e8d4beef9405773883726cd7664
Oliinyk Maksym <maxim@oliinyk.com.ua>
2025-06-14 13:27:56 -0400

    Added Ethereum Network fallback connection

--- src/core/tasks/connect/eth_network.py
+++ src/core/tasks/connect/eth_network.py
@@ -4,25 +4,30 @@ from airflow.exceptions import AirflowException
 from src.core.utils.connection import EthereumConnection
 
 load_dotenv()
-URL = os.getenv("URL")
+URLS = os.getenv("URLS").split(",") if os.getenv("URLS") else []
 
-eth_conn = EthereumConnection(URL=URL)
-w3 = eth_conn.get_connection()
+eth_conn = EthereumConnection(URLs=URLS)
 
 def connect_to_ethereum(**kwargs):
     try:
-        if not w3:
-            raise AirflowException('Web3 instance is not initialized.')
+        w3 = eth_conn.get_connection()
         
         if not w3.is_connected():
-            raise AirflowException('Failed to connect to Ethereum network.')
-        
+            eth_conn.rotate_endpoint()
+            w3 = eth_conn.get_connection()
+            
         block_number = w3.eth.block_number
         if not isinstance(block_number, int) or block_number < 0:
-            raise AirflowException(f"Invalid block number received: {block_number}")
+            eth_conn.rotate_endpoint()
+            raise ValueError("Invalid block number")
         
-        kwargs['ti'].xcom_push(key='node_url', value=URL)
+        kwargs['ti'].xcom_push(key='node_url', value=eth_conn.URLs[eth_conn.current_url_index])
+        kwargs['ti'].xcom_push(key='node_url_index', value=eth_conn.current_url_index)
         return block_number
 
+    except ValueError as e:
+        eth_conn.rotate_endpoint()
+        return connect_to_ethereum(**kwargs)
+        
     except Exception as e:
-        raise AirflowException(f"Error: {e}")
+        raise AirflowException(f"All endpoints failed. Last error: {str(e)}")
--- src/core/utils/connection.py
+++ src/core/utils/connection.py
@@ -1,15 +1,33 @@
 from web3 import Web3
+from typing import List
 
 class EthereumConnection:
-    def __init__(self, URL):
-        self.URL = URL
-        self.w3 = Web3(Web3.HTTPProvider(self.URL))
-        if not self.w3.is_connected():
-            raise ConnectionError("Failed to connect to Ethereum node")
+    def __init__(self, URLs: List[str]):
+        self.URLs = URLs
+        self.current_url_index = 0
+        self.w3 = None
+        self._connect()
+
+    def _connect(self):
+        for i, url in enumerate(self.URLs[self.current_url_index:], start=self.current_url_index):
+            self.w3 = Web3(Web3.HTTPProvider(url))
+            if self.w3.is_connected():
+                self.current_url_index = i
+                return
+        
+        raise ConnectionError(f'Failed to connect to all Ethereum nodes {self.URLs}')
 
     def get_connection(self):
+        if not self.w3:
+            return False
         return self.w3
     
     def is_connected(self):
+        if not self.w3:
+            return False
         return self.w3.is_connected()
+    
+    def rotate_endpoint(self):
+        self.current_url_index = (self.current_url_index + 1) % len(self.URLs)
+        self._connect()
 
